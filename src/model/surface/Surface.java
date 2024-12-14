bc2977c4096bd80c6697241aa8127df3294bb4f4
dinh1duc2anh3 <dinh1duc2anh3@gmail.com>
2024-12-14 13:54:28 +0700

    topic/surface

---
+++ src/model/surface/Surface.java
@@ -0,0 +1,34 @@
+package Surface;
+
+
+public class Surface {
+	 private double staticCoefficient;
+	 private double kineticCoefficient;
+	
+	 public Surface(double staticCoefficient, double kineticCoefficient) {
+		 //note : đưa ra lỗi nếu như staticCoeeficient <= kineticCoefficient
+		 
+		 this.staticCoefficient = staticCoefficient;
+		 this.kineticCoefficient = kineticCoefficient;
+	 }
+	
+
+
+	public double getStaticCoefficient() {
+        return staticCoefficient;
+    }
+
+    public double getKineticCoefficient() {
+        return kineticCoefficient;
+    }
+    
+	public void setStaticCoefficient(double staticCoefficient) {
+		this.staticCoefficient = staticCoefficient;
+	}
+
+	public void setKineticCoefficient(double kineticCoefficient) {
+		this.kineticCoefficient = kineticCoefficient;
+	}
+	
+	 
+}
