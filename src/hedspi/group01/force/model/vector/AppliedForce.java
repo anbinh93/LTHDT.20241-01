9f7bafcb64977aa9837807203f60795f11729966
anbinh93 <an.nb225591@sis.hust.edu.vn>
2024-12-17 22:58:50 +0700

    update model: vector

--- src/hedspi/group01/force/model/vector/AppliedForce.java
+++ src/hedspi/group01/force/model/vector/AppliedForce.java
@@ -1,8 +1,40 @@
 package hedspi.group01.force.model.vector;
+
+/**
+ * This class represents an applied force that actor applies on the MainObject
+ * 
+ * 
+ */
 public class AppliedForce extends Force {
-    public AppliedForce(double magnitude, double direction) {
-        super(magnitude, direction);
+
+    /**
+     * Holds the absolute maximum value of applied force
+     */
+    public static final double ABS_MAX_AFORCE = 500;
+
+    /**
+     * Class constructor specifying value of this AppliedForce
+     *
+     * @param value The value of this AppliedForce
+     */
+    public AppliedForce(double value) {
+        super(value);
     }
 
-   
+    /**
+     * Changes the value of this AppliedForce
+     */
+    @Override
+    public void setValue(double value) {
+        // When force exceeds its maximum and minimum threshold, set it to this
+        // threshold
+        if (value > ABS_MAX_AFORCE) {
+            setValue(ABS_MAX_AFORCE);
+        } else if (value < -ABS_MAX_AFORCE) {
+            setValue(-ABS_MAX_AFORCE);
+        } else {
+            super.setValue(value);
+        }
+    }
+
 }
--- src/hedspi/group01/force/model/vector/Force.java
+++ src/hedspi/group01/force/model/vector/Force.java
@@ -1,21 +1,26 @@
 package hedspi.group01.force.model.vector;
 
 public class Force extends HorizontalVector {
-	public Force(double magnitude, double direction) {
-		super(magnitude, direction);
-	}
-	
-	public double calculateAcceleration(double mass) {
-        // Gia tốc = Lực / Khối lượng
-        return getMagnitude() * getDirection() / mass;
+    public Force(double value) {
+        super(value);
     }
-	
-//	@Override
-//    public String toString() {
-//        return "Force{" +
-//               "magnitude=" + getMagnitude() +
-//               ", direction=" + (getDirection() > 0 ? "Right" : "Left") +
-//               '}';
-//    }
-	
+
+    public double calculateAcceleration(double mass) {
+        // Gia tốc = Lực / Khối lượng
+        return getValue() / mass;
+    }
+
+    // Tính tổng của 2 lực
+    public static Force sumTwoForce(Force force1, Force force2) {
+        Force netForce = new Force(force1.getValue() + force2.getValue());
+        netForce.updateValueDirection();
+        return netForce;
+    }
+    @Override
+    public String toString() {
+        return "Force{" +
+               "value=" + getValue() +
+               ", direction=" + (getDirection() ? "Right" : "Left") +
+               '}';
+    }
 }
