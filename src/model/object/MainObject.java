e698fd244e54a16ce9e086a1644605679eaa8362
Cao Duc Anh <ducanh127nps@gmail.com>
2024-12-24 13:06:11 +0700

    Removed unused attribute (y pos)

--- src/model/object/MainObject.java
+++ src/model/object/MainObject.java
@@ -2,7 +2,7 @@ package model.object;
 public abstract class MainObject {
     private int id;
     private float xpos;
-    private float ypos;
+    //private float ypos;
     private String type;
     private float friction_coefficient; //Reserved for object's surface friction coefficient
     private float mass;
@@ -10,10 +10,10 @@ public abstract class MainObject {
     private float accel;
 
 
-    public MainObject(int id, int xpos, int ypos, String type, float friction_coefficient, float mass, float vel, float accel) {
+    public MainObject(int id, int xpos, String type, float friction_coefficient, float mass, float vel, float accel) {
         this.id = id;
         this.xpos = xpos;
-        this.ypos = ypos;
+        //this.ypos = ypos;
         this.type = type;
         this.friction_coefficient = friction_coefficient;
         this.mass = mass;
@@ -21,10 +21,10 @@ public abstract class MainObject {
         this.accel = accel;
     }
 
-    public MainObject(int id, int xpos, int ypos, String type, float mass, float vel, float accel) {
+    public MainObject(int id, int xpos, String type, float mass, float vel, float accel) {
         this.id = id;
         this.xpos = xpos;
-        this.ypos = ypos;
+        //this.ypos = ypos;
         this.type = type;
         this.mass = mass;
         this.vel = vel;
@@ -39,9 +39,9 @@ public abstract class MainObject {
         return xpos;
     }
 
-    public float getYpos() {
-        return ypos;
-    }
+    //public float getYpos() {
+    //    return ypos;
+    //}
 
     public String getType() {
         return type;
@@ -67,9 +67,9 @@ public abstract class MainObject {
         this.xpos = xpos;
     }
 
-    public void setypos(float ypos) {
-        this.ypos = ypos;
-    }
+    //public void setypos(float ypos) {
+    //    this.ypos = ypos;
+    //}
 
     public void setMass(float mass) {
         this.mass = mass;
